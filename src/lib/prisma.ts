25163b6d59cc6c520bc010fee831f037b9a77fcf
Bicky Kumar Yadav <bicky.yadav@z1tech.com>
2026-01-14 15:57:03 +0530

    fix: explicit import path to generated prisma client file

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/client'
+import { PrismaClient } from '../generated/client/client'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
